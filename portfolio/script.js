eadb7bba40f11a54bfbb230f1bdb77cd8f000f39
Rajib Mandal <rajibmandal782003@gmail.com>
2024-05-23 23:52:19 +0530

    Initial commit

---
+++ portfolio/script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    // You can add your logic to handle form submission, such as sending data to a server
+    // For simplicity, this example does nothing
+  });
+  
